c7440ecfd7f0dbb48afbe73b55af9aea86615694
Brian Scholer <Brian.Scholer@rga.com>
2017-01-26 16:45:14 -0500

    Remove repeated code, call Get-WmiIbject with splatting, add help, change output type to TimeSpan, update README

--- Get-Uptime.ps1
+++ Get-Uptime.ps1
@@ -1,46 +1,38 @@
-Function Get-uptime
+Function Get-Uptime
 {
-	
 	[CmdletBinding()]
-	[OutputType([int])]
+	[OutputType([System.TimeSpan])]
 	Param
 	(
-		# Param1 help description
-		[Parameter(Mandatory = $false,
-				   Position = 0)]
-		[string[]]$ComputerName
-		
+		[Parameter(
+            Mandatory = $false,
+	        Position = 0
+        )]
+        [ValidateNotNullOrEmpty()]
+		[String[]]
+        $ComputerName
 	)
 	
-	if ($ComputerName -ne $null)
-	{
-		
-		
-		
-		# Try one or more commands
-		try
-		{
-			$os = Get-wmiobject -ComputerName $ComputerName win32_operatingsystem -ErrorAction stop
-			$uptime = (get-date) - ($os.ConvertTodateTime($os.lastbootuptime))
-			$display = $uptime.days
-			write-output $display
-		}
-		# Catch specific types of exceptions thrown by one of those commands
-        catch {
-            Write-Warning "Machine $Computername is offline" -ForegroundColor Yellow -BackgroundColor Black
-        }
+    $params = @{
+        Class = 'Win32_OperatingSystem'
+    }
+    if ($ComputerName) {
+        $params.ComputerName = $ComputerName
+    }
 
-	}
+	$os = Get-WmiObject @params
 	
-	
-	else
-	{
+    (Get-Date) - ($os.ConvertTodateTime($os.LastBootUpTime))
 		
-		$os = Get-wmiobject win32_operatingsystem
-		$uptime = (get-date) - ($os.ConvertTodateTime($os.lastbootuptime))
-		$display = $uptime.days
-		write-output $display
-		
-		
-	}
+<#
+.SYNOPSIS
+Gets the uptime for one or more computers.
+
+.PARAMETER ComputerName
+The name(s) of the computer(s) for which uptime is needed. If not specified, the local computer is checked.
+
+.LINK
+https://github.com/itadder/Get-Uptime
+
+#>
 }
--- README.md
+++ README.md
@@ -1,4 +1,49 @@
 # Get-Uptime
-Get-Uptime -ComputerName localhost
-It will display Computer uptime for local or remote host.
-It is a simple not fancy script.
+
+See `Get-Help Get-Uptime`:
+
+
+```
+
+NAME
+    Get-Uptime
+    
+SYNOPSIS
+    Gets the uptime for one or more computers.
+    
+SYNTAX
+    Get-Uptime [[-ComputerName] <String[]>] [<CommonParameters>]
+    
+    
+DESCRIPTION
+    
+
+PARAMETERS
+    -ComputerName <String[]>
+        The name(s) of the computer(s) for which uptime is needed. If not specified, the local computer is checked.
+        
+        Required?                    false
+        Position?                    1
+        Default value                
+        Accept pipeline input?       false
+        Accept wildcard characters?  false
+        
+    <CommonParameters>
+        This cmdlet supports the common parameters: Verbose, Debug,
+        ErrorAction, ErrorVariable, WarningAction, WarningVariable,
+        OutBuffer, PipelineVariable, and OutVariable. For more information, see 
+        about_CommonParameters (http://go.microsoft.com/fwlink/?LinkID=113216). 
+    
+INPUTS
+    
+OUTPUTS
+    System.TimeSpan
+        
+    
+    
+    
+    
+RELATED LINKS
+    https://github.com/itadder/Get-Uptime
+
+```
